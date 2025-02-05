c1082c437620b8ea9ca8917fd235dcb35b20fa39
Shawn Mathew <smathew@NETOPSMVLT.accessgeneral.com>
2025-02-05 17:03:21 -0500

    Create Home Page with Tabs

--- lib/main.dart
+++ lib/main.dart
@@ -35,3 +35,43 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage ({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  
+  @override 
+  void initState(){
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+       title: const Text ('Digital Pet States'),
+       bottom: TabBar(
+          controller: _tabController,
+          tabs: const [
+            Tab(text: "Bruno"),
+            Tab(text: "Lola"),
+            Tab(text: "Charlie"),
+              ],
+            ), 
+          ),
+      ),
+  }
+}
